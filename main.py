452598dfc667445df46daf0a2b170a0dbb92703a
jmaddock <skimaddster@gmail.com>
2013-02-20 22:10:29 -0800

    python datahandler (no output)

---
+++ main.py
@@ -0,0 +1,63 @@
+import urllib, urllib2, time, hashlib
+import json as simplejson
+
+API_KEY = "xdc8ttebmyukvrzsxtby8hqc"
+API_SECRET = "zgQj9ZTbwa"
+
+# Pre : input a dictionary with the required params (artist name + filters required)
+# Post: outputs a JSON formatted dictionary w/ requested data
+# this method will probably be called multiple times for each viz in order to get info
+# about both the "root" artist and the "leaf" artists
+def getData(base = 'http://api.rovicorp.com/search/v2.1/music/search?',
+            key = API_KEY,
+            secret = API_SECRET,
+            return_format = 'json',
+            country = 'us',
+            include = '',
+            params = {}):
+    
+    params['apikey'] = key
+    params['format'] = return_format
+    params['country'] = country
+    params['sig'] = sig()
+    url = base + urllib.urlencode(params) + "&include="+include   
+    try: 
+        json = (convert(safeGet(url)))
+        print pretty(json)
+        return json
+    except:
+        return None
+# Pre : input JSON files in a dictionary, possibly multiple
+# Post: returns JSON file formatted for the d3 basic network
+def treeFormat(jsonIn, nodeList = {}):
+    json = jsonIn['searchResponse']['results'][1]['name']
+
+# same method as above, but formats to encode geo-data
+#TODO: def mapFormat():
+    
+def convert(restful):
+    jsonresult = restful.read()
+    return simplejson.loads(jsonresult)
+
+def pretty(obj):
+    return simplejson.dumps(obj, sort_keys=True, indent=2)
+    
+def safeGet(url):
+    try:
+        return urllib2.urlopen(url)
+    except urllib2.URLError, e:
+        if hasattr(e, 'reason'):
+            print 'We failed to reach a server.'
+            print 'Reason: ', e.reason
+        elif hasattr(e, 'code'):
+            print 'The server couldn\'t fulfill the request.'
+            print 'Error code: ', e.code
+        return None
+
+def sig():
+        timestamp = int(time.time())
+        m = hashlib.md5()
+        m.update(API_KEY)
+        m.update(API_SECRET)
+        m.update(str(timestamp))
+        return m.hexdigest()
