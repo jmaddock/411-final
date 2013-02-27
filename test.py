77dcaf1b670d3cbea33d85931ed651dbe2d7ea35
jmaddock <skimaddster@gmail.com>
2013-02-26 21:47:26 -0800

    updated/functional test.py

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import main
 
+#this should probably be 'all' in the final
 include = 'Aliases,AssociatedWith,CollaboratorWith,Contemporaries,Followers,GroupMembers,Influencers,MemberOf,Similars'
+nodeList = ['aliases', 'associatedWith', 'collaboratorWith', 'contemporaries', 'followers', 'groupMembers', 'influencers', 'memberOf', 'similars']
 query = 'eric clapton'
 params = {'endpoint':'music',
           'entitytype':'artist',
           'query':query,}
-main.getData(params = params, include = include)
+json = main.getData(params = params, include = include)
+main.treeFormat(json, nodeList)
