3a601f81b1b99aa7631db553ec8a88bbc5b39479
jmaddock <skimaddster@gmail.com>
2013-03-11 14:47:53 -0700

    updated labeling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import urllib, urllib2, time, hashlib
 import json as simplejson
 
+
 API_KEY = "xdc8ttebmyukvrzsxtby8hqc"
 API_SECRET = "zgQj9ZTbwa"
 
@@ -29,22 +30,79 @@ def getData(base = 'http://api.rovicorp.com/search/v2.1/music/search?',
 # Pre : input JSON files in a dictionary, possibly multiple
 # Post: returns JSON file formatted for the d3 basic network
 def treeFormat(jsonIn, nodeList = []):
+    params = {'endpoint':'music',
+              'entitytype':'artist',}
+    include = 'Moods,MusicBio,MusicStyles,Themes'
+    includeList = ['moods','musicBio','musicStyles','themes']
     result = {}
     json = jsonIn['searchResponse']['results'][0]['name']
-    result['name'] = json['aliases'][0] #TODO: fix this
+    result['name'] = json['name']
     result['children'] = []
     for  x in nodeList:
         result['children'].append({'name':x, 'children':[]})
     for x in result['children']:
         if json[x['name']]:
+            count = 0
             for y in json[(x['name'])]:
-                if x['name'] == 'aliases':
-                    x['children'].append({'name':y})
+                if x['name'] == 'aliases': #TODO: start here, start including data
+                    result['Aliases'] = y
                 else:
-                    x['children'].append({'name':y['name']})
+                    params['query'] = y['name']
+                    z = getData(params = params, include = include)['searchResponse']['results'][0]['name']
+                    if 'weight' in y:
+                        x['children'].append({'name':y['name'],
+                                              'Weight':y['weight'],
+                                              'children':[]})
+                        for a in includeList:
+                            try:
+                                x['children'][count][formatTitle(a)] = z[a][0]['name']
+                            except:
+                                print 'working...'
+#                                              'moods':z['moods'][0],
+#                                              'musicGeneres':z['musicGenres'][0],
+#                                              'musicStyles':z['musicStyles'][0],
+#                                              'themes':z['themes'][0],})
+                    else:
+                        x['children'].append({'name':y['name']})
+                    count += 1
+    for x in result['children']:
+        x['name'] = formatTitle(x['name'])
+    print pretty(result)
     return simplejson.dumps(result)
-#    for x in result['children']:
-#        x = json[]
+
+def expandTree(jsonIn, artist):
+    jsonIn
+    
+def formatTitle(input):
+    if input == 'aliases':
+        return "Aliases"
+    elif input == 'associatedWith':
+        return "Associated Artists"
+    elif input == 'collaboratorWith':
+        return "Callaborated With"
+    elif input == 'contemporaries':
+        return "Contemporaries"
+    elif input == "followers":
+        return "Followers"
+    elif input == 'groupMembers':
+        return "Group Members"
+    elif input == 'influencers':
+        return "Influencers"
+    elif input == 'memberOf':
+        return "Member Of"
+    elif input == 'similars':
+        return "Similar Artists"
+    elif input == 'moods':
+        return "Mood"
+    elif input == 'musicBio':
+        return "Music Bio"
+    elif input == 'musicStyles':
+        return "Style"
+    elif input == 'themes':
+        return "Theme"
+    else:
+        return input
+    
 
 # same method as above, but formats to encode geo-data
 #TODO: def mapFormat():
@@ -75,3 +133,4 @@ def sig():
         m.update(API_SECRET)
         m.update(str(timestamp))
         return m.hexdigest()
+    
