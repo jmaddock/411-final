d455b852f032c4f1b66112cf2533d844c4e736f1
jmaddock <skimaddster@gmail.com>
2013-03-11 22:55:49 -0700

    updated for latest main

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
-import main
+#import data
 
 #this should probably be 'all' in the final
-include = 'Aliases,AssociatedWith,CollaboratorWith,Contemporaries,Followers,GroupMembers,Influencers,MemberOf,Similars'
-nodeList = ['aliases', 'associatedWith', 'collaboratorWith', 'contemporaries', 'followers', 'groupMembers', 'influencers', 'memberOf', 'similars']
-query = 'eric clapton'
-params = {'endpoint':'music',
-          'entitytype':'artist',
-          'query':query,}
-json = main.getData(params = params, include = include)
-main.treeFormat(json, nodeList)
+def run():
+    include = 'Aliases,AssociatedWith,CollaboratorWith,Contemporaries,Followers,GroupMembers,Influencers,MemberOf,Similars,Images,Moods,MusicBio,MusicStyles,Themes,Web'
+    nodeList = ['associatedWith', 'collaboratorWith', 'contemporaries', 'followers', 'groupMembers', 'influencers', 'similars']
+    query = 'eric clapton'
+    params = {'endpoint':'music',
+              'entitytype':'artist',
+              'query':query,}
+    json = data.getData(params = params, include = include)
+    return data.treeFormat(json, nodeList)
+
+def main():
+    run()
+
+if __name__ == "__main__":
+    main()
